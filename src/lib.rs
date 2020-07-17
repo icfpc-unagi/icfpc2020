aadbbb5e1912b99aceb617dd71e7ddf123e45b0e
imos <173752+imos@users.noreply.github.com>
2020-07-18 08:19:54 +0900

    Implement sender.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-use std::collections::*;
-use std::rc::Rc;
-use num::*;
-
 pub mod modulation;
 pub mod parser;
+pub mod sender;
---
+++ src/sender.rs
@@ -0,0 +1,44 @@
+use http_body::Body as _;
+use hyper::{Client, Request, Method, Body, StatusCode};
+use hyper_tls::HttpsConnector;
+use tokio::runtime::Runtime;
+
+async fn send_async(s: String) -> String {
+    let https = HttpsConnector::new();
+    let client = Client::builder()
+        .build::<_, hyper::Body>(https);
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri("https://icfpc2020-api.testkontur.ru/aliens/send?apiKey=ffb372cc81824c9e820112787bc93329")
+        .body(Body::from(s)).unwrap();
+    match client.request(req).await {
+        Ok(mut res) => {
+            let mut cs = vec!();
+            while let Some(chunk) = res.body_mut().data().await {
+                match chunk {
+                    Ok(c) => {
+                        cs.append(&mut c.to_vec());
+                    }
+                    Err(e) => panic!("{}", e),
+                }
+            }
+            let s = String::from_utf8(cs).unwrap();
+            match res.status() {
+                StatusCode::OK => return s,
+                _ => panic!("HTTP code: {}\nBody: {}", res.status(), s),
+            }
+        },
+        Err(err) => panic!("Unexpected server response:\n{}", err),
+    }
+}
+
+fn send(s: &str) -> String {
+    let s = String::from(s);
+    Runtime::new().expect("Failed to create tokio runtime").block_on(send_async(s))
+}
+
+#[test]
+#[ignore]
+fn test_send() {
+  assert_eq!(send("010"), "1101000");
+}
