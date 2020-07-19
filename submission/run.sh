f20e96ee689d255687c2b757b760ff120f6adaaf
Kentaro IMAJO <173752+imos@users.noreply.github.com>
2020-07-20 06:39:31 +0900

    Read /AI on release (#67)

--- submission/run.sh
+++ submission/run.sh
@@ -6,13 +6,7 @@ export ICFPC_API_HOST=$1
 export ICFPC_API_KEY=$2
 export JUDGE_SERVER=1
 
-free -h
-for f in /sys/fs/cgroup/memory/*; do
-	echo $f
-	cat $f || echo 'failed to read'
-	echo
-done
-cat /proc/cpuinfo
+source ./AI
 
-./app ${EXTRA_FLASG:-} "$@" || \
+./app "${AI_FLAGS[@]}" ${EXTRA_FLAGS:-} "$@" || \
 	echo "ERROR: exit_code=$?"
