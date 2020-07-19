7177b1ae1024cb6ab63cb1436c69680112b0eadb
Kentaro IMAJO <173752+imos@users.noreply.github.com>
2020-07-20 06:05:42 +0900

    Allow empty commit on release (#66)

--- script/create-submission.sh
+++ script/create-submission.sh
@@ -23,6 +23,6 @@ bash script/build-submission.sh
 
 pushd build/submission
 git add -A
-git commit -am "${DATETIME}-${COMMIT_ID} #release"$'\n'"${COMMIT_MSG}"
+git commit --allow-empty -am "${DATETIME}-${COMMIT_ID} #release"$'\n'"${COMMIT_MSG}"
 git push --set-upstream origin "submission"
 popd
