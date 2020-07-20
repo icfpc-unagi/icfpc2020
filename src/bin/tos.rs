ed4adaa78e0ad01ac89360e49a64d82ccd2ec7f3
Toshiki Kataoka <tos.lunar@gmail.com>
2020-07-20 16:00:53 +0900

    wip
    Co-authored-by: sulume <sulume@gmail.com>

--- src/bin/tos.rs
+++ src/bin/tos.rs
@@ -1,4 +1,6 @@
 use app::client::*;
+// use std;
+use std::convert::*;
 
 fn run() {
 	let server_url = std::env::args().nth(1).unwrap();
@@ -8,11 +10,60 @@ fn run() {
 		return;
 	}
 	let player_key = std::env::args().nth(2).unwrap();
-	client.join(&player_key);
-	let mut resp = client.start(446, 0, 0, 1);
-	let id = resp.state.ships.iter().find_map(|s| if s.role == resp.info.role { Some(s.id) } else { None }).unwrap();
+	let mut resp = client.join(&player_key);
+	let power = 64;
+	let cool = 8;
+	let life = 1;
+	resp = client.start(resp.info.ability.potential - power * 4 - cool * 12 - life * 2, power, cool, life);
+
+	let mut dx: i32 = std::env::var("DX").unwrap().parse().unwrap();
+	let dy: i32 = std::env::var("DY").unwrap().parse().unwrap();
 	while resp.stage != 2 {
-		resp = client.command(&[Command::Accelerate(id, (0, -1))]);
+		let mut myship = None;
+		for ship in resp.state.ships.iter() {
+			if ship.role != resp.info.role {
+				continue;
+			}
+			myship = Some(ship.clone());
+		}
+		let myship = myship.unwrap();
+
+		let mut commands = vec![];
+		{
+			// anti gravity
+			let (x, y) = myship.pos;
+			println!("{}, {}", x, y);
+			// assert_eq!(myship.v, (0, 0));
+			println!("{:?}", myship.v);
+			let (gx, gy) = gravity(x, y);
+			println!("{}, {}", gx, gy);
+			Command::Accelerate(myship.id, (-gx, -gy));
+		}
+		
+		let shoot_now = myship.heat -myship.status.cool <= myship.max_heat;
+		if shoot_now {
+			commands.push(Command::Shoot(1, (myship.pos.0 + dx, myship.pos.1 + dy), 64, None));
+			dx+=1;
+		}
+
+		// !!!
+		resp = client.command(&commands);
+		// !!!
+
+		if shoot_now {
+			// println!("{}", resp);
+			println!("shoot!!!!!!!");
+			for ship in resp.state.ships.iter() {
+				for cmd in ship.commands.iter() {
+					match cmd {
+						Command::Shoot(_, (x,y), _, Some((impact, four))) => {
+							println!("({},{}) impact={}, four={}", x - ship.pos.0,y - ship.pos.1, impact, four);
+						},
+						_=> {},
+					}
+				}
+			}
+		}
 	}
 }
 
@@ -23,4 +74,24 @@ fn main() {
 		.spawn(run)
 		.unwrap()
 		.join();
+}
+
+fn gravity(x: i32, y: i32) -> (i32, i32) {
+	(
+		if x.abs() >= y.abs() {
+			if x < 0 {
+				1
+			} else {
+				-1
+			}
+		} else { 0 },
+
+		if x.abs() <= y.abs() {
+			if y < 0 {
+				1
+			} else {
+				-1
+			}
+		} else { 0 },
+	)
 }
