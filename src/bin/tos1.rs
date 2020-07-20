e4051ec4f08460e8b871f2489a8594982263d23d
Toshiki Kataoka <tos.lunar@gmail.com>
2020-07-20 19:32:15 +0900

    tmp

--- src/bin/tos1.rs
+++ src/bin/tos1.rs
@@ -13,8 +13,14 @@ fn run() {
 	}
 	let player_key = std::env::args().nth(2).unwrap();
 	let mut resp = client.join(&player_key);
-	let power = 32;
-	let cool = 0;
+	let role = resp.info.role;
+	println!("{}", role);
+	let power = if role == 0 {
+		100
+	} else {
+		0
+	};
+	let cool = 4;
 	let life = 1;
 	resp = client.start(resp.info.ability.potential - power * 4 - cool * 12 - life * 2, power, cool, life);
 
@@ -22,62 +28,73 @@ fn run() {
 	// let dy: i32 = std::env::var("DY").unwrap().parse().unwrap();
 	let mut dx = 0;
 	let mut dy = 0;
+	let mut shoot_flag = false;
+	let mut teki_energy = 0;
 	while resp.stage != 2 {
 		let mut myship = None;
+		let mut tekiship = None;
 		for ship in resp.state.ships.iter() {
 			if ship.role != resp.info.role {
-				continue;
+				tekiship = Some(ship.clone());
 			}
 			myship = Some(ship.clone());
 		}
 		let myship = myship.unwrap();
+		let tekiship = tekiship.unwrap();
+
+
+		if shoot_flag {
+			// println!("{}", resp);
+			println!("S H O O O O O O O T ? ? ? ? ? ? ?");
+			for cmd in myship.commands.iter() {
+				match cmd {
+					Command::Shoot(_, (x,y), p, Some((impact, four))) => {
+						println!("S H O O O O O O O T ! ! ! ! ! ! !");
+						println!("[shoot], {}, {}, {}, {}, {}, {}, {}, {}",
+							x - myship.pos.0, y - myship.pos.1,
+							x - tekiship.pos.0, y - tekiship.pos.1,
+							tekiship.status.energy - teki_energy,
+							p, impact, four);
+					},
+					_=> {},
+				}
+			}
+		}
+		teki_energy = tekiship.status.energy;
 
 		let mut commands = vec![];
 
 		{
 			// anti gravity
 			let (x, y) = myship.pos;
-			// println!("{}, {}", x, y);
+			println!("{}, {}", x, y);
 			// assert_eq!(myship.v, (0, 0));
-			// println!("{:?}", myship.v);
+			println!("{:?}", myship.v);
 			let (gx, gy) = gravity(x, y);
-			// println!("{}, {}", gx, gy);
+			println!("{}, {}", gx, gy);
 			commands.push(
 				Command::Accelerate(myship.id, gravity(x, y))
 			);
 		}
 		
-		let shoot_now = myship.heat -myship.status.cool + 16 <= myship.max_heat;
-		if shoot_now {
+		shoot_flag = role == 0;
+		if shoot_flag {
+			println!("S H O O O O O O O T ? ? ?");
+			println!("{}, {}, {}", myship.heat, myship.status.cool, myship.max_heat);
 			let fix = resp.info.role;
-			// dx = rng.gen_range(0, 20) + fix;
-			dx = rng.gen_range(32, 96) + fix;
+			dx = rng.gen_range(0, 2);
+			dy = dx + rng.gen_range(0, 2);
 			// dy = rng.gen_range(0, 20) + fix;
-			dy = 0;
 			let shoot_power = power;
-			commands.push(Command::Shoot(1, (myship.pos.0 + dx, myship.pos.1 + dy), shoot_power, None));
+			let (mut x, mut y) = tekiship.pos;
+			x += tekiship.v.0;
+			y += tekiship.v.1;
+			commands.push(Command::Shoot(myship.id, (x + dx, y + dy), shoot_power, None));
 		}
 
 		// !!!
 		resp = client.command(&commands);
 		// !!!
-
-		if shoot_now {
-			// println!("{}", resp);
-			println!("S H O O O O O O O T ! ! ! ! ! ! !");
-			for ship in resp.state.ships.iter() {
-				for cmd in ship.commands.iter() {
-					match cmd {
-						Command::Shoot(_, (x,y), p, Some((impact, four))) => {
-							println!("_, x, y, power, impact, four");
-							println!("[shoot], {}, {}, {}, {}, {}",
-								x - ship.pos.0,y - ship.pos.1, p, impact, four);
-						},
-						_=> {},
-					}
-				}
-			}
-		}
 	}
 }
 
